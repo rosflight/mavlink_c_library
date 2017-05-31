74977b2cb2d9022110881e06daeebc1edfec4ca4
James Jackson <superjax08@gmail.com>
2017-05-30 18:51:43 -0600

    autogenerated headers for rev https://github.com/BYU-MAGICC/mavlink/tree/e2ad0aea4b41a23edea01279121e2dc535bd2103

--- rosflight/testsuite.h
+++ rosflight/testsuite.h
@@ -623,6 +623,50 @@ static void mavlink_test_rosflight_version(uint8_t system_id, uint8_t component_
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 }
 
+static void mavlink_test_rosflight_aux_cmd(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+	mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+	mavlink_rosflight_aux_cmd_t packet_in = {
+		{ 17.0, 18.0, 19.0, 20.0, 21.0, 22.0, 23.0, 24.0 },101
+    };
+	mavlink_rosflight_aux_cmd_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        	packet1.type_array = packet_in.type_array;
+        
+        	mav_array_memcpy(packet1.aux_cmd_array, packet_in.aux_cmd_array, sizeof(float)*8);
+        
+
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_rosflight_aux_cmd_encode(system_id, component_id, &msg, &packet1);
+	mavlink_msg_rosflight_aux_cmd_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_rosflight_aux_cmd_pack(system_id, component_id, &msg , packet1.type_array , packet1.aux_cmd_array );
+	mavlink_msg_rosflight_aux_cmd_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_rosflight_aux_cmd_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.type_array , packet1.aux_cmd_array );
+	mavlink_msg_rosflight_aux_cmd_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+        	comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+	mavlink_msg_rosflight_aux_cmd_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_rosflight_aux_cmd_send(MAVLINK_COMM_1 , packet1.type_array , packet1.aux_cmd_array );
+	mavlink_msg_rosflight_aux_cmd_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
 static void mavlink_test_rosflight(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
 	mavlink_test_offboard_control(system_id, component_id, last_msg);
@@ -638,6 +682,7 @@ static void mavlink_test_rosflight(uint8_t system_id, uint8_t component_id, mavl
 	mavlink_test_rosflight_output_raw(system_id, component_id, last_msg);
 	mavlink_test_rosflight_status(system_id, component_id, last_msg);
 	mavlink_test_rosflight_version(system_id, component_id, last_msg);
+	mavlink_test_rosflight_aux_cmd(system_id, component_id, last_msg);
 }
 
 #ifdef __cplusplus
