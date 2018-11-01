4d616b2ae17d9b11243c396895e1cb1bd7fc968a
James Jackson <superjax08@gmail.com>
2018-11-01 15:51:05 -0600

    autogenerated headers for rev https://github.com/rosflight/mavlink/tree/be6401202a51982c311fa2e04a0e9cf13bef4309

--- rosflight/testsuite.h
+++ rosflight/testsuite.h
@@ -726,6 +726,52 @@ static void mavlink_test_rosflight_ins(uint8_t system_id, uint8_t component_id, 
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 }
 
+static void mavlink_test_rosflight_quaternion(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+	mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+	mavlink_rosflight_quaternion_t packet_in = {
+		17.0,45.0,73.0,101.0
+    };
+	mavlink_rosflight_quaternion_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        	packet1.qw = packet_in.qw;
+        	packet1.qx = packet_in.qx;
+        	packet1.qy = packet_in.qy;
+        	packet1.qz = packet_in.qz;
+        
+        
+
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_rosflight_quaternion_encode(system_id, component_id, &msg, &packet1);
+	mavlink_msg_rosflight_quaternion_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_rosflight_quaternion_pack(system_id, component_id, &msg , packet1.qw , packet1.qx , packet1.qy , packet1.qz );
+	mavlink_msg_rosflight_quaternion_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_rosflight_quaternion_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.qw , packet1.qx , packet1.qy , packet1.qz );
+	mavlink_msg_rosflight_quaternion_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+        	comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+	mavlink_msg_rosflight_quaternion_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_rosflight_quaternion_send(MAVLINK_COMM_1 , packet1.qw , packet1.qx , packet1.qy , packet1.qz );
+	mavlink_msg_rosflight_quaternion_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
 static void mavlink_test_rosflight(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
 	mavlink_test_offboard_control(system_id, component_id, last_msg);
@@ -743,6 +789,7 @@ static void mavlink_test_rosflight(uint8_t system_id, uint8_t component_id, mavl
 	mavlink_test_rosflight_version(system_id, component_id, last_msg);
 	mavlink_test_rosflight_aux_cmd(system_id, component_id, last_msg);
 	mavlink_test_rosflight_ins(system_id, component_id, last_msg);
+	mavlink_test_rosflight_quaternion(system_id, component_id, last_msg);
 }
 
 #ifdef __cplusplus
